ae461cb856c1ab75595f1f4d2437fd37d7c03c75
jinwoong <baseball0020@gmail.com>
2022-06-13 22:51:03 +0900

    final

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import TempHum from "./components/TempHum/TempHum";
 import { refresh } from "./action/index";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [temp, setTemp] = useState("0°C");
   const [hum, setHum] = useState("0%");
